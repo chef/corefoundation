99157c61e99271f34bf0668785a8bc2b4c65c014
Frederick Cheung <frederick.cheung@gmail.com>
2012-11-03 13:03:03 +0000

    document Boolean

--- lib/corefoundation/boolean.rb
+++ lib/corefoundation/boolean.rb
@@ -2,10 +2,18 @@ module CF
   attach_variable 'kCFBooleanTrue', :pointer
   attach_variable 'kCFBooleanFalse', :pointer
   attach_function 'CFBooleanGetValue', [:pointer], :uchar
+
+  # Wrapper for CFBooleanRef
+  # Typically you use the CF::Boolean::TRUE and CF::Boolean::FALSE constants
+  #
   class Boolean < Base
     register_type("CFBoolean")
     TRUE = new(CF.kCFBooleanTrue)
     FALSE = new(CF.kCFBooleanFalse)
+
+    # returns a ruby true/false value
+    #
+    # @return [Boolean]
     def value
       CF.CFBooleanGetValue(self) != 0
     end
