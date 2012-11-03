fb840317c4a79be05ad6b23cabd84065bfc2d192
Frederick Cheung <frederick.cheung@gmail.com>
2012-11-03 13:08:03 +0000

    document CF::date

--- lib/corefoundation/date.rb
+++ lib/corefoundation/date.rb
@@ -6,12 +6,23 @@ module CF
   attach_function :CFDateGetAbsoluteTime, [:cfdateref], :cfabsolutetime
   attach_variable :kCFAbsoluteTimeIntervalSince1970, :double
 
+  # Wrapper for CFDateRef
+  #
+  #
   class Date < Base
     register_type 'CFDate'
+
+    # constructs a CF::Date from a ruby time
+    #
+    # @param [Time] time
+    # @return [CF::Date] a CF::Date instance that will be released on garbage collection
     def self.from_time(time)
-      new(CF.CFDateCreate(nil, time.to_f - CF.kCFAbsoluteTimeIntervalSince1970,)).release_on_gc
+      new(CF.CFDateCreate(nil, time.to_f - CF.kCFAbsoluteTimeIntervalSince1970)).release_on_gc
     end
 
+    # returns a ruby Time instance corresponding to the same point in time
+    #
+    # @return [Time]
     def to_time
       Time.at(CF.CFDateGetAbsoluteTime(self) + CF.kCFAbsoluteTimeIntervalSince1970)
     end
