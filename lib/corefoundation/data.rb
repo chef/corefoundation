1fc951605799d8b95f254492f9ea09f0b79cad6a
Frederick Cheung <frederick.cheung@gmail.com>
2012-11-03 13:05:27 +0000

    document CFData

--- lib/corefoundation/data.rb
+++ lib/corefoundation/data.rb
@@ -5,17 +5,29 @@ module CF
   attach_function 'CFDataGetLength', [:cfdataref], :cfindex  
   attach_function 'CFDataGetBytePtr', [:cfdataref], :pointer
 
+  # Wrapper for CFData
+  #
+  #
   class Data < Base
     register_type("CFData")
+
+    # Creates a CFData from a ruby string
+    # @param [String] s the string to use
+    # @return [CF::Data]
     def self.from_string(s)
       new(CF.CFDataCreate(nil, s, s.bytesize)).release_on_gc
     end
 
+    # Creates a ruby string from the wrapped data. The encoding will always be ASCII_8BIT
+    #
+    # @return [String]
     def to_s
       ptr = CF.CFDataGetBytePtr(self)
       ptr.read_string(CF.CFDataGetLength(self)).force_encoding(Encoding::ASCII_8BIT)
     end
 
+    # The size in bytes of the CFData
+    # @return [Integer]
     def size
       CF.CFDataGetLength(self)
     end
