3ddcfda617deb0ed35170f7c8d5155e9accde163
Frederick Cheung <frederick.cheung@gmail.com>
2012-11-03 13:14:11 +0000

    document cfdictionary

--- lib/corefoundation/dictionary.rb
+++ lib/corefoundation/dictionary.rb
@@ -30,12 +30,27 @@ module CF
   callback :each_applier, [:pointer, :pointer, :pointer], :void
 
   attach_function :CFDictionaryApplyFunction, [:cfdictionaryref, :each_applier, :pointer], :void
+
+  # Wrapper class for CFDictionary. It implements Enumerable.
+  #
+  # Values returned by the accessor methods or yielded by the block are retained and marked as releasable on garbage collection
+  # This means you can safely use the returned values even if the CFDictionary itself has been destroyed
+  #
   class Dictionary < Base
     register_type("CFDictionary")
     include Enumerable
+
+    # Return a new, empty mutable CF::Dictionary
+    #
+    #
+    # @return [CF::Dictionary]
     def self.mutable
       new(CF.CFDictionaryCreateMutable nil, 0, CF.kCFTypeDictionaryKeyCallBacks.to_ptr, CF.kCFTypeDictionaryValueCallBacks.to_ptr).release_on_gc
     end
+
+    # Iterates over the array yielding the value to the block
+    # The value is wrapped in the appropriate CF::Base subclass and retained (but marked for releasing upon garbage collection)
+    # @return self
 
     def each
       callback = lambda do |key, value, _|
@@ -45,6 +60,10 @@ module CF
       self
     end
 
+    # Returns the value associated with the key, or nil
+    # The key should be a CF::Base instance. As a convenience, instances of string will be converted to CF::String
+    # @param [CF::Base, String] key the key to access
+    # @return [CF::Base, nil]
 
     def [](key)
       key = CF::String.from_string(key) if key.is_a?(::String)
@@ -53,6 +72,11 @@ module CF
       raw.null? ? nil : self.class.typecast(raw).retain.release_on_gc
     end
 
+    # Sets the value associated with the key, or nil
+    # The key should be a CF::Base instance. As a convenience, instances of string will be converted to CF::String
+    # @param [CF::Base, String] key the key to access
+    # @param [CF::Base] value the value to set
+    # @return [CF::Base] the value that was set
     def []=(key, value)
       key = CF::String.from_string(key) if key.is_a?(::String)
       self.class.check_cftype(key)
@@ -61,16 +85,26 @@ module CF
       value
     end
 
+    # Adds the key value pairs from the argument to self
+    #
+    # @param [CF::Dictionary] other
+    # @return self
     def merge!(other)
       other.each do |k,v|
         self[k] = v
       end
+      self
     end
 
+    # Returns the number of key value pairs in the dictionary
+    # @return [Integer]
     def length
       CF.CFDictionaryGetCount(self)
     end
 
+    # Returns a ruby hash constructed by calling `to_ruby` on each key and value
+    #
+    # @return [Hash]
     def to_ruby
       Hash[collect {|k,v| [k.to_ruby, v.to_ruby]}]
     end
