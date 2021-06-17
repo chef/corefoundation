810cdc5c0426a698506cd2456274a832b2284c3f
Amulya <18429813+logwolvy@users.noreply.github.com>
2021-06-17 08:39:26 +0530

    Update formatting
    Signed-off-by: Amulya <atomer@progress.com>

--- lib/corefoundation/preferences.rb
+++ lib/corefoundation/preferences.rb
@@ -14,19 +14,12 @@ module CF
   class Preferences
     class << self
       def get(key, application_id)
-        plist_ref = CF.CFPreferencesCopyAppValue(
-          key.to_cf,
-          application_id.to_cf
-        )
+        plist_ref = CF.CFPreferencesCopyAppValue(key.to_cf, application_id.to_cf)
         CF::Base.typecast(plist_ref).to_ruby
       end
 
       def set(key, value, application_id)
-        CF.CFPreferencesSetAppValue(
-          key.to_cf,
-          value.to_cf,
-          application_id.to_cf
-        )
+        CF.CFPreferencesSetAppValue(key.to_cf, value.to_cf, application_id.to_cf)
         CF.CFPreferencesAppSynchronize(application_id.to_cf)
       end
     end
