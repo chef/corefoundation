542a9856f9146213fff744443897bc32a3d6a189
rishichawda <rishichawda@users.noreply.github.com>
2021-07-28 13:00:54 +0530

    bump rspec to latest, migrate tests
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- spec/boolean_spec.rb
+++ spec/boolean_spec.rb
@@ -6,16 +6,16 @@ describe CF do
   describe CF::Boolean do
     describe 'value' do
       it 'should return true for CF::Boolean::TRUE' do
-        CF::Boolean::TRUE.value.should == true
+        expect(CF::Boolean::TRUE.value).to eq(true)
       end
       it 'should return false for CF::Boolean::FALSE' do
-        CF::Boolean::FALSE.value.should == false
+        expect(CF::Boolean::FALSE.value).to eq(false)
       end
     end
 
     describe 'to_ruby' do
       it 'should behave like value' do
-        CF::Boolean::FALSE.to_ruby.should == false
+        expect(CF::Boolean::FALSE.to_ruby).to eq(false)
       end
     end
 
