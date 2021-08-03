e67991c9406ad34cc41c9cfa4481abfe2e5c0fbf
rishichawda <rishichawda@users.noreply.github.com>
2021-08-03 19:11:27 +0530

    feat(preferences.rb): update preferences interface
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- lib/corefoundation/exceptions.rb
+++ lib/corefoundation/exceptions.rb
@@ -1,6 +1,16 @@
 module CF
   module Exceptions
-    # Raised when abstract method is not overridden
-    class MethodNotImplemented < StandardError; end
+    # Raised when the preference value couldn't be found for a domain.
+    class PreferenceDoesNotExist < StandardError
+      def initialize(key, domain, hostname)
+        super("Returned NULL value for \"#{key}\" in \"#{domain}\", hostname: #{hostname}")
+      end
+    end
+    # Raised when the preference value failed to write.
+    class PreferenceSyncFailed < StandardError
+      def initialize(key, domain, hostname)
+        super("Couldn't write preference value for \"#{key}\" in \"#{domain}\", hostname: #{hostname}")
+      end
+    end
   end
 end
--- lib/corefoundation/preferences.rb
+++ lib/corefoundation/preferences.rb
@@ -1,6 +1,3 @@
-# Maps Preferences Utilities
-# Documentation at https://developer.apple.com/documentation/corefoundation/preferences_utilities
-
 module CF
   typedef :pointer, :cfpropertylistref
   typedef :cfstringref, :key
@@ -13,16 +10,18 @@ module CF
   attach_variable 'kCFPreferencesAnyUser', :cfstringref
   attach_variable 'kCFPreferencesCurrentHost', :cfstringref
   attach_variable 'kCFPreferencesAnyHost', :cfstringref
-  attach_variable 'kCFPreferencesAnyApplication', :cfstringref
 
   attach_function 'CFPreferencesCopyAppValue', [:key, :application_id], :cfpropertylistref
   attach_function 'CFPreferencesCopyValue', [:key, :application_id, :username, :hostname], :cfpropertylistref
-  attach_function 'CFPreferencesCopyKeyList', [:application_id, :username, :hostname], :cfarrayref
 
   attach_function 'CFPreferencesSetAppValue', [:key, :value, :application_id], :void
   attach_function 'CFPreferencesSetValue', [:key, :value, :application_id, :username, :hostname], :void
+
   attach_function 'CFPreferencesAppSynchronize', [:application_id], :bool
 
+  # Interface to the preference utilities from Corefoundation.framework.
+  # Documentation at https://developer.apple.com/documentation/corefoundation/preferences_utilities
+  #
   class Preferences
     using CF::Refinements
 
@@ -32,38 +31,101 @@ module CF
     ALL_HOSTS = CF.kCFPreferencesAnyHost
 
     class << self
+      # Returns the output from `CFPreferencesCopyValue` call after converting it to ruby type.
+      #
+      # @param [String] key Preference key to read
+      # @param [String] application_id Preference domain for the key
+      # @param [String, Symbol] username Domain user (current, any or a specific user)
+      # @param [String, Symbol] hostname Hostname (current, all hosts or a specific host)
+      #
+      # @return [VALUE] Preference value returned from the `CFPreferencesCopyValue` call.
+      #
       def get(key, application_id, username = nil, hostname = nil)
-        # TODO: Check usecase for `CFPreferencesCopyAppValue`
-
-        plist_ref = if username && hostname
-                      CF.CFPreferencesCopyValue(key.to_cf, application_id.to_cf, arg_to_cf(username), arg_to_cf(hostname))
-                    else
+        plist_ref = if username.nil? or hostname.nil?
                       CF.CFPreferencesCopyAppValue(key.to_cf, application_id.to_cf)
+                    else
+                      CF.CFPreferencesCopyValue(
+                        key.to_cf,
+                        application_id.to_cf,
+                        arg_to_cf(username),
+                        arg_to_cf(hostname)
+                      )
                     end
         CF::Base.typecast(plist_ref).to_ruby unless plist_ref.null?
       end
 
+      # Calls the {#self.get} method and raise a `PreferenceDoesNotExist` error if `nil` is returned.
+      #
+      # @param [String] key Preference key to read
+      # @param [String] application_id Preference domain for the key
+      # @param [String, Symbol] username Domain user (current, any or a specific user)
+      # @param [String, Symbol] hostname Hostname (current, all hosts or a specific host)
+      #
+      # @raise [PreferenceDoesNotExist] If returned value is nil.
+      #
+      # @return [VALUE] Preference value returned from the {#self.get} method call.
+      #
       def get!(key, application_id, username = nil, hostname = nil)
-        get(key, application_id, username, hostname) || (raise CF::Error, "Preference key `#{key}` not found")
+        value = get(key, application_id, username, hostname)
+        if value.nil?
+          raise(CF::Exceptions::PreferenceDoesNotExist.new(key, application_id, hostname))
+        else
+          value
+        end
       end
 
-      def set(key, value, application_id, username = CURRENT_USER, hostname = CURRENT_HOST)
-        CF.CFPreferencesSetValue(key.to_cf, value.to_cf, application_id.to_cf, arg_to_cf(username), arg_to_cf(hostname))
+      # Set the value for preference domain using `CFPreferencesSetValue`.
+      #
+      # @param [String] key Preference key
+      # @param [Integer, Float, String, TrueClass, FalseClass, Hash, Array] value Preference value
+      # @param [String] application_id Preference domain
+      # @param [String, Symbol] username Domain user (current, any or a specific user)
+      # @param [String, Symbol] hostname Hostname (current, all hosts or a specific host)
+      #
+      # @return [TrueClass, FalseClass] Returns true if preference was successfully written to storage, otherwise false.
+      #
+      def set(key, value, application_id, username = nil, hostname = nil)
+        if username.nil? or hostname.nil?
+          CF.CFPreferencesSetAppValue(
+            key.to_cf,
+            arg_to_cf(value),
+            application_id.to_cf
+          )
+        else
+          CF.CFPreferencesSetValue(
+            key.to_cf,
+            arg_to_cf(value),
+            application_id.to_cf,
+            arg_to_cf(username),
+            arg_to_cf(hostname)
+          )
+        end
         CF.CFPreferencesAppSynchronize(application_id.to_cf)
       end
 
-      def list_keys(application_id, username, hostname)
-        arr_ref = CF.CFPreferencesCopyKeyList(
-          application_id.to_cf,
-          arg_to_cf(username),
-          arg_to_cf(hostname)
-        )
-        CF::Array.new(arr_ref).to_ruby
+      # Calls the {#self.set} method and raise a `PreferenceSyncFailed` error if `false` is returned.
+      #
+      # @param [String] key Preference key to write
+      # @param [String] application_id Preference domain for the key
+      # @param [String, Symbol] username Domain user (current, any or a specific user)
+      # @param [String, Symbol] hostname Hostname (current, all hosts or a specific host)
+      #
+      # @raise [PreferenceSyncFailed] If {#self.set} call returned false.
+      #
+      # @return [VALUE] Returns nil if preference value is successfully written.
+      #
+      def set!(key, value, application_id, username = nil, hostname = nil)
+        raise(CF::Exceptions::PreferenceSyncFailed.new(key, application_id, hostname)) unless set(key, value, application_id, username, hostname)
       end
 
       private
-
-      # Check for constants CURRENT_USER, ALL_HOSTS etc.
+      # Convert an object from ruby to cf type.
+      #
+      # @param [VALUE, CFType] arg A ruby or corefoundation object.
+      #
+      # @return [CFType] A wrapped CF object.
+      #
+      # @visiblity private
       def arg_to_cf(arg)
         arg.respond_to?(:to_cf) ? arg.to_cf : arg
       end
