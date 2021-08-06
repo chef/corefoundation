0e2ad6661c6b40bc6db8c3fcd70c4773a87b5591
rishichawda <rishichawda@users.noreply.github.com>
2021-08-06 16:37:20 +0530

    test(preferences_spec): add tests for preference interface
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

---
+++ spec/preferences_spec.rb
@@ -0,0 +1,85 @@
+require "spec_helper"
+
+describe CF::Preferences do
+  before do
+    @validDomain = "TestValidDomain"
+    @invalidDomain = "TestInvalidDomain"
+    @validKey = "ValidTestKey"
+    @invalidKey = "InvalidTestKey"
+    @value = "TestValue"
+  end
+
+  after(:each) do
+    CF::Preferences.set!(@validKey, nil, @validDomain)
+    CF::Preferences.set!(@invalidKey, nil, @validDomain)
+  end
+
+  describe "self.set" do
+    context "when called with valid domain/default pair" do
+      it "writes correct value and returns true" do
+        expect(CF::Preferences.set(@validKey, @value, @validDomain)).to eq true
+        expect(CF::Preferences.get(@validKey, @validDomain)).to eq @value
+      end
+    end
+  end
+
+  describe "self.set!" do
+    context "when called with valid domain/default pair" do
+      it "writes correct value and returns nil" do
+        expect(CF::Preferences.set!(@validKey, @value, @validDomain)).to be_nil
+        expect(CF::Preferences.get(@validKey, @validDomain)).to eq @value
+      end
+    end
+  end
+
+  describe "self.get" do
+    before do
+      CF::Preferences.set(@validKey, @value, @validDomain)
+    end
+
+    context "when called with valid domain/default pair" do
+      it "returns value" do
+        expect(CF::Preferences.get(@validKey, @validDomain)).to eq @value
+      end
+    end
+
+    context "when called with invalid domain/default pair" do  
+      it "returns nil" do
+        expect(CF::Preferences.get(@validKey, @invalidDomain)).to be_nil
+      end
+    end
+  end
+
+  describe "self.get!" do
+    context "when called with valid domain/default pair" do
+      it "returns value" do
+        expect(CF::Preferences.set!(@validKey, @value, @validDomain)).to be_nil
+        expect(CF::Preferences.get!(@validKey, @validDomain)).to eq @value
+      end
+    end
+
+    context "when called with invalid domain/default pair" do  
+      it "raise an error" do
+        expect { CF::Preferences.get!(@validKey, @invalidDomain) }.to raise_error CF::Exceptions::PreferenceDoesNotExist
+      end
+    end
+  end
+
+  describe "self.valid_key?" do
+    before do
+      CF::Preferences.set!(@validKey, @value, @validDomain)
+    end
+
+    context "when called for a valid domain/default pair" do
+      it 'returns true' do
+        expect(CF::Preferences.valid_key?(@validKey, @validDomain)).to eq true
+      end
+    end
+
+    context "when called for an invalid domain/default pair" do
+      it "returns false" do
+        expect(CF::Preferences.valid_key?(@invalidKey, @validDomain)).to eq false
+      end
+    end
+  end
+end
