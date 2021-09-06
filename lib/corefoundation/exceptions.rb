290b3590bbc64c533990adb06432541b5f063ad3
rishichawda <rishichawda@users.noreply.github.com>
2021-09-06 17:44:49 +0530

    make hostname optional
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- lib/corefoundation/exceptions.rb
+++ lib/corefoundation/exceptions.rb
@@ -2,14 +2,14 @@ module CF
   module Exceptions
     # Raised when the preference value couldn't be found for a domain.
     class PreferenceDoesNotExist < StandardError
-      def initialize(key, domain, hostname)
-        super("Returned NULL value for \"#{key}\" in \"#{domain}\", hostname: #{hostname}")
+      def initialize(key, domain, hostname = nil)
+        super("Returned NULL value for \"#{key}\" in \"#{domain}\"#{", hostname: #{hostname}" if hostname}")
       end
     end
     # Raised when the preference value failed to write.
     class PreferenceSyncFailed < StandardError
-      def initialize(key, domain, hostname)
-        super("Couldn't write preference value for \"#{key}\" in \"#{domain}\", hostname: #{hostname}")
+      def initialize(key, domain, hostname = nil)
+        super("Couldn't write preference value for \"#{key}\" in \"#{domain}\"#{", hostname: #{hostname}" if hostname}")
       end
     end
   end
