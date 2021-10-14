5fb44f006a2846bfdffea57e59b0996c2cbb9827
Avinash Raghuthu <avinash.raghuthu@gmail.com>
2021-10-14 14:14:37 +0530

    wrote identity parsing code

--- lib/corefoundation.rb
+++ lib/corefoundation.rb
@@ -39,28 +39,25 @@ module CF
   typedef :pointer, :cfdataref
 end
 
-require_relative 'corefoundation/memory'
-require_relative 'corefoundation/register'
-require_relative 'corefoundation/base'
-require_relative 'corefoundation/null'
-require_relative 'corefoundation/range'
-require_relative 'corefoundation/string'
-require_relative 'corefoundation/array'
-require_relative 'corefoundation/boolean'
-require_relative 'corefoundation/data'
-require_relative 'corefoundation/dictionary'
-require_relative 'corefoundation/number'
-require_relative 'corefoundation/date'
-require_relative 'corefoundation/preferences'
-require_relative 'corefoundation/extensions'
+# require_relative 'corefoundation/base'
+# require_relative 'corefoundation/null'
+# require_relative 'corefoundation/string'
+# require_relative 'corefoundation/array'
+# require_relative 'corefoundation/boolean'
+# require_relative 'corefoundation/data'
+# require_relative 'corefoundation/dictionary'
+# require_relative 'corefoundation/number'
+# require_relative 'corefoundation/date'
+# require_relative 'corefoundation/preferences'
+# require_relative 'corefoundation/extensions'
 require_relative 'corefoundation/security'
 
-# REVIEW: Include patches from module
-Integer.include CF::CoreExtensions::Integer
-Float.include CF::CoreExtensions::Float
-Array.include CF::CoreExtensions::Array
-TrueClass.include CF::CoreExtensions::TrueClass
-FalseClass.include CF::CoreExtensions::FalseClass
-String.include CF::CoreExtensions::String
-Time.include CF::CoreExtensions::Time
-Hash.include CF::CoreExtensions::Hash
+# # REVIEW: Include patches from module
+# Integer.include CF::CoreExtensions::Integer
+# Float.include CF::CoreExtensions::Float
+# Array.include CF::CoreExtensions::Array
+# TrueClass.include CF::CoreExtensions::TrueClass
+# FalseClass.include CF::CoreExtensions::FalseClass
+# String.include CF::CoreExtensions::String
+# Time.include CF::CoreExtensions::Time
+# Hash.include CF::CoreExtensions::Hash
--- lib/corefoundation/security.rb
+++ lib/corefoundation/security.rb
@@ -27,6 +27,7 @@ module CF
     attach_variable 'kSecMatchLimitOne', :pointer
     attach_variable 'kSecAttrSerialNumber', :pointer
     attach_variable 'kSecReturnAttributes', :pointer
+    attach_variable 'kSecMatchLimitAll', :pointer
 
     attach_function 'SecItemExport' , [:secItemOrArray, :secExternalFormat,
                                          :flags, :keyParams, :exportedData], :OSStatus
@@ -42,8 +43,9 @@ module CF
         def self.export(attr_label, passphrase)
             exportedData = FFI::MemoryPointer.new :pointer
             itemMatchResult = search_cert(attr_label)
+            # binding.pry
             keyParams = SecItemImportExportKeyParameters.new
-            keyParams[:passphrase] = "qwertyasdf".to_cf
+            keyParams[:passphrase] = passphrase.to_cf
             puts "keyParams", keyParams.values
             status = export_util(itemMatchResult, :formatPKCS12,0,keyParams, exportedData)
             desc = CF.SecCopyErrorMessageString(status, CF::NULL)
@@ -66,31 +68,71 @@ module CF
             result = FFI::MemoryPointer.new :pointer
             query = {}
             query[CF::Base.typecast(CF.kSecClass)] = CF::Base.typecast(CF.kSecClassIdentity)
-            query[CF::Base.typecast(CF.kSecMatchLimit)] = CF::Base.typecast(CF.kSecMatchLimitOne)
-            # query[CF::Base.typecast(CF.kSecAttrLabel)] = attr_label
+            # query[CF::Base.typecast(CF.kSecMatchLimit)] = CF::Base.typecast(CF.kSecMatchLimitOne)
+            query[CF::Base.typecast(CF.kSecAttrLabel)] = attr_label
+            # query[CF::Base.typecast(CF.kSecMatchLimit)] = CF::Base.typecast(CF.kSecMatchLimitAll)
             # query[CF::Base.typecast(CF.kSecAttrSubject)] = "/C=BE/O=Test/OU=Test/CN=securtytest".to_cf
             # query[CF::Base.typecast(CF.kSecAttrSubject)] = "securtytest".to_cf
-            query[CF::Base.typecast(CF.kSecAttrSerialNumber)] = '12345'.to_cf
+            # query[CF::Base.typecast(CF.kSecAttrSerialNumber)] = '12345'.to_cf
             query[CF::Base.typecast(CF.kSecReturnRef)] = true
+            # query[CF::Base.typecast(CF.kSecReturnAttributes)] = true
            return search_item(query.to_cf, result)
         end
 
-        def self.find_identity()
+        def self.search_identity(query, result)
+            status = CF.SecItemCopyMatching(query, result)
+            desc = CF.SecCopyErrorMessageString(status, CF::NULL)
+            itemMatchResult = result.read_pointer
+            puts "status: #{status} - #{CF::Base.typecast(desc).to_s}"
+            # puts "itemMatchResult:", itemMatchResult
+            # puts "typeofreturn", CF::String.new(CF.CFCopyDescription(itemMatchResult)).to_s
+            return itemMatchResult
+        end
+
+        def self.find_identity(attr_label, passphrase)
             result = FFI::MemoryPointer.new :pointer
             query = {}
             query[CF::Base.typecast(CF.kSecClass)] = CF::Base.typecast(CF.kSecClassIdentity)
-            query[CF::Base.typecast(CF.kSecMatchLimit)] = CF::Base.typecast(CF.kSecMatchLimitOne)
+            query[CF::Base.typecast(CF.kSecMatchLimit)] = CF::Base.typecast(CF.kSecMatchLimitAll)
             query[CF::Base.typecast(CF.kSecReturnAttributes)] = true
             query[CF::Base.typecast(CF.kSecReturnRef)] = true
-            resultRefs = search_item(query.to_cf, result)
-            identitiesArr =  CF::Array.new resultRefs
+            resultRefs = search_identity(query.to_cf, result)
+
+            # identitiesArr =  CF::Array.new resultRefs
+            identitiesArr =  CF::Base.typecast(resultRefs)
+            # binding.pry
+            # puts identitiesArr.to_ruby
+            # puts identitiesArr.class
             identitiesCount = identitiesArr.length
             puts "Identities count", identitiesCount
-            CF.CFArrayGetValueAtIndex(identitiesArr,  CF::Number.from_i(1))
-            # identitiesArr.each do |identityRef|
-            #     identityDict = CF::Dictionary.new identityRef
-            #     puts identityDict
-            # end
+            
+            itemMatchResult = nil
+            identitiesArr.each do |identityRef|
+                if identityRef['labl'] == attr_label.to_cf
+                    itemMatchResult = identityRef
+                end
+            end 
+            puts itemMatchResult 
+           
+            # itemMatchResult = itemMatchResult["v_Ref"]
+            secItem = CF.CFDictionaryGetValue(itemMatchResult, "v_Ref".to_cf)
+           
+            # puts secItem
+            secIdentitiesArr =  CF::Array.mutable 
+            secIdentitiesArr <<  secItem
+            keyParams = SecItemImportExportKeyParameters.new
+            keyParams[:passphrase] = passphrase.to_cf
+            # puts "keyParams", keyParams.values
+            binding.pry
+            exportedData = FFI::MemoryPointer.new :pointer
+            status = export_util(secIdentitiesArr.to_ptr, :formatPKCS12,0,keyParams, exportedData)
+            puts status
+            desc = CF.SecCopyErrorMessageString(status, CF::NULL)
+            puts "status: #{status} - #{CF::Base.typecast(desc).to_s}"
+            # desc = CF.SecCopyErrorMessageString(status, CF::NULL)
+            # puts "status: #{status} - #{CF::Base.typecast(desc).to_s}"
+            # puts CF.show(exportedData.read_pointer)
+            # File.open("exportcert.p12", 'w+') { |file| file.write(CF::Base.typecast(exportedData.read_pointer).to_s) }
         end
     end
 end
