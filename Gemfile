155150a0e70c296a5ec759826efe633a18058926
rishichawda <rishichawda@users.noreply.github.com>
2022-02-08 21:01:52 +0530

    add code coverage stats
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -1,2 +1,7 @@
 source 'https://rubygems.org'
 gemspec
+
+group(:test) do
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,16 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  track_files '{lib}/**/*.rb'
+  enable_coverage :branch
+  refuse_coverage_drop :line, :branch
+  minimum_coverage line: 95, branch: 80
+  formatter SimpleCov::Formatter::Console
+end
 
 $: << File.dirname(__FILE__) + '/../lib'
 
